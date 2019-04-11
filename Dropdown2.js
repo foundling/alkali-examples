181d6b95f1130fb2533a278de7dfde5602d5d186
Alex Ramsdell <alexramsdell@gmail.com>
2019-04-11 17:39:23 -0400

    major cleanup

--- Dropdown2.js
+++ Dropdown2.js
@@ -9,6 +9,7 @@ const {
   LI, 
   Button,
   UL, 
+  Span,
 
   Renderer, 
   Variable,
@@ -18,29 +19,27 @@ const {
 } = alkali
 
 /*
- * items[]: list of items to render
  * value: 
  * events
  *
- * data: 
- * ui: Element for list item
+ *  maxSelections: Number,
+ *  items: [],
+ *  searchInputConstructor,
+ *  listItemConstructor,
+ *
  */
 
 /* selection and filter data */
-const activeSelections = reactive([])
-const searchFilter = new VString('')
-
-/* default constructors */
-const DefaultItem = LI.with('.btn.btn-sm')
-const DefaultInput = Div.with('.search-input-container', [
-  Input.with('.search-input', { placeholder: 'Reasons for Rejection'}),
-  Button.with('.search-input-icon.fa.fa-search')
-])
 
 class Dropdown extends Element.with({ 
 
   children: [ 
-    UL.with('.dre-dropdown-list') 
+    Div.with('.dre-dd-container', {
+      children: [
+        Div.with('.dre-dd-search-container'),
+        Div.with('.dre-dd-list-container')
+      ]
+    })
   ] 
 
 }) {
@@ -55,82 +54,100 @@ class Dropdown extends Element.with({
    *  dre-dd-removed
    */ 
 
-  created({ data = {}, ui = {} } = {}) {
+  created({ items, hidden, ItemConstructor, InputConstructor }) {
 
-    const { items, value } = data
-    const { Item, Input } = ui
-
-    /* ensure types are alkali */ 
+    /* ensure data is reactive */
     this.items = items instanceof Variable ? items : reactive(items || [])
-    console.log(this.items)
+
+    /* state */
+    this.hidden = hidden || false
+    this.searchFilter = new VString('')
+    this.activeSelections = reactive([])
 
     /* pick constructors */
-    this.Item = Item || DefaultItem
-    this.Input = Input || DefaultInput
+    this.Item = (ItemConstructor || LI).with('.dre-dd-list-item', {
+      onkeyup: (e) => {
+        this.searchFilter.put(e.target.value)
+      }
+    })
+    this.Input = (InputConstructor || Div).with('.dre-dd-search-input', {
+      children: [
+        Input.with('.dre-dd-search-input'),
+        Span.with('.fa.fa-caret-down.dre-dd-search-input-icon')
+      ]
+    })
 
   }
   attached() {
 
-    this.inputContainer = new this.Input({
-      onkeyup(e) {
-        searchFilter.put(e.target.value)
-      }
-    })
+    const [dropdownContainer] = [...this.children]
+    const [searchContainer, listContainer] = [...dropdownContainer.children]
+    this.dropdownContainer = dropdownContainer
+    this.searchContainer = searchContainer
+    this.listContainer = listContainer
 
-    this.ul = this.children[0]
-    const listItems = this.items.map((itemText, index) => new this.Item('.dre-dropdown-list-item', {
-      textContent: itemText,
-      id: `dre-dd-item-${index}`,
-      classes: {
-        hidden: searchFilter.to(searchFilter => {
-          if (searchFilter.length == 0)
-            return false
-          return !Boolean(itemText.match(searchFilter))
-        }),
-        selected: activeSelections.to(selections => selections[index])
-      }
+    this.searchContainer.append(new this.Input({
+      onkeyup: (e) => {
+        this.searchFilter.put(e.target.value)
+      }  
     }))
+    this.listContainer.append(
+      new UL('.dre-dd-list', [
+        this.items.map((itemText, index) => new this.Item('.dre-dd-list-item', {
+          textContent: itemText,
+          id: `dre-dd-item-${index}`,
+          classes: {
+            hidden: this.searchFilter.to(searchFilter => {
+              if (searchFilter.length == 0)
+                return false
+              return !Boolean(itemText.match(searchFilter))
+            }),
+            selected: this.activeSelections.to(selections => selections[index])
+          }
+        }))
 
-    this.prepend(this.inputContainer)
-    this.ul.append(...listItems)
+      ])
+    )
 
-    this.ul.addEventListener('click', this.onSelect.bind(this))
+    this.listContainer.addEventListener('click', this.onSelect.bind(this))
     
   }
   onSelect({ target }) {
 
-    let children = [...this.ul.children]
-    let index = children.indexOf(target)
+    let [ul] = [...this.listContainer.children]
+    let listItems = [...ul.children]
+    let index = listItems.indexOf(target)
 
     if (index < 0) 
       return
 
-    activeSelections.set(index, activeSelections.get(index) == null ? true : null )
+    this.activeSelections.set(index, this.activeSelections.get(index) == null ? true : null )
 
+  }
+
+  toggle() {
+  }
+
+  show() {
+  }
+
+  hide() {
   }
 
 }
+//export default Dropdown.defineElement('dre-dd')
 
 const items = reactive(['one','two','three'])
 const reasonsRejected = reactive([])
-const D = Dropdown.defineElement('dre-dropdown')
-const MyListItem = LI.with('.rejection-reason', {
-  onclick() {
-    console.log('toggle rating!')
-  }
-})
+const D = Dropdown.defineElement('dre-dd')
+
 
 const dd = new D({
-  data: {
-    items
-  },
-  ui: {
-    Item: MyListItem
-  }
+  items,
+  hidden: false,
+  //Item: MyListItem
 })
 document.body.appendChild(dd)
 
-
 //document.body.appendChild(new alkali.Div(searchFilter))
 //document.body.appendChild(new alkali.Div(activeSelections.to(JSON.stringify)))
-///export default Dropdown.defineElement('dre-dropdown')
