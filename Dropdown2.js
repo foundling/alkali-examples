178c8c663e3080496b00e1c0b1d3715513021bad
Alex Ramsdell <alexramsdell@gmail.com>
2019-04-12 17:15:43 -0400

    use ready event and props instead of attached and this

--- Dropdown2.js
+++ Dropdown2.js
@@ -19,61 +19,96 @@ const {
 
 } = alkali
 
+
 class Dropdown extends Element { 
 
-  created({ parent=document.body, open=true, corner='topright', items, closed=false, ItemConstructor, InputConstructor }) {
+  created(props) { 
+    const {
+      items, 
+      values, 
+      parent=document.body, 
+      open=true, 
+      corner='topright', 
+      closed=false,
+      placeholder='',
+      maxSelections=null,
+      ItemConstructor,
+      InputConstructor 
+    } = props
+
+
+    this.props = props
+    // API: do i allow a single item? Array.isArray
+    // HOW TO ACCEPT A PROMISE?
 
     /* ensure data is reactive */
+    props.items = items instanceof Variable ? items : reactive(items || [])
+    props.values = values instanceof Variable ? values : reactive(values || [])
+    props.maxSelections = maxSelections || props.items.get('length') 
 
-    // API: do i allow a single item? Array.isArray
-    this.items = items instanceof Variable ? items : reactive(items || [])
-     console.log('items in created: ', this.items)
+    // work out what happens when you reach max. 
+    // proposal: 
+    // if it's one, just change the selection.
+    // if multiple, need to deselect before selecting a new item 
 
     /* state */
-    this.open = reactive(open)
-    this.parent = parent
-    this.corner = corner
-    this.searchFilter = new VString('')
-    this.selectedByIndex = reactive({})
-    this.selectedValues = this.selectedByIndex.to(obj => {
-      return _.pickBy(obj, (x) => x != null)
+    props.open = reactive(open)
+    props.placeholder = reactive(placeholder)
+    props.parent = parent
+    props.corner = corner
+    props.searchFilter = new VString('')
+    props.selectedByIndex = reactive(this.initializeSelected(props.values.valueOf(), props.items.valueOf())) // map from index to value
+    props.selectedValues = props.selectedByIndex.to(obj => { // array of just values
+      return Object.keys(obj).reduce((arr, key) => {
+        return obj[key] ? arr.concat(obj[key]) : arr
+      }, [])
     })
 
     /* pick constructors */
     // API: custom constructors must have at least the default children
-    this.Item = ItemConstructor || Button.with('.dre-dd-list-item')
-    this.Input = (InputConstructor || Div.with('.dre-dd-search-input', {
+    props.Item = ItemConstructor || Button.with('.dre-dd-list-item')
+    props.Input = (InputConstructor || Div.with('.dre-dd-search-input', {
       onkeyup: (e) => {
-        this.searchFilter.put(e.target.value)
+        props.searchFilter.put(e.target.value)
       },
       children: [
-        Input.with('.dre-dd-search-input'),
-        Span.with('.fa.fa-caret-down.dre-dd-search-input-icon')
+        Input.with('.dre-dd-search-input-el', {
+          placeholder: props.placeholder
+        }),
+        Span.with('.fa.fa-search.dre-dd-search-input-icon')
       ]
     }))
 
   }
-  attached() {
+  ready(props) {
 
+    const selectedList = props.items.to(items => items.map((item, index) => {
+      return new Button('.dre-dd-selection', {
+        hidden: props.selectedByIndex.to(selectedMap => selectedMap[index])
+      })
+    }))
     this.append(new (Div.with('.dre-dd-container', {
       children: [
+        Div.with('.dre-dd-selections-container', [
+            ...props.selectedValues.to(selected => selected.map(value => new props.Item('.selected-item', [value])))
+        ]),
         Div.with('.dre-dd-search-container'),
         Div.with('.dre-dd-list-container', {
           classes: {
-            hidden: not(this.open)
+            hidden: not(props.open)
           }
         })
       ]
     })))
 
     const [dropdownContainer] = [...this.children]
-    const [searchContainer, listContainer] = [...dropdownContainer.children]
+    const [selectionsContainer, searchContainer, listContainer] = [...dropdownContainer.children]
     this.dropdownContainer = dropdownContainer
     this.searchContainer = searchContainer
     this.listContainer = listContainer
-    this.searchInput = new this.Input({
+    this.searchInput = new props.Input({
       onkeyup: (e) => {
-        this.searchFilter.put(e.target.value)
+        props.searchFilter.put(e.target.value)
       }  
     })
 
@@ -82,7 +117,7 @@ class Dropdown extends Element {
     // clean this up
     this.listContainer.append(
       new Div('.dre-dd-list', [
-        this.items.map((itemText, index) => new this.Item('.dre-dd-list-item', {
+        props.items.map((itemText, index) => new props.Item({
           textContent: itemText,
           id: `dre-dd-item-${index}`,
           classes: {
@@ -91,7 +126,7 @@ class Dropdown extends Element {
                 return false
               return !Boolean(itemText.toLowerCase().includes(searchFilter.toLowerCase()))
             }),
-            selected: this.selectedByIndex.to(selections => selections[index])
+            selected: props.selectedByIndex.to(selections => selections[index])
           }
         }))
 
@@ -99,7 +134,7 @@ class Dropdown extends Element {
     )
 
     this.listContainer.addEventListener('click', this.onSelect.bind(this))
-    this.position(this.parent)
+    this.position(props.parent)
 
     this.handleOutsideClicks = (e) => {
       const dropdown = this
@@ -113,28 +148,36 @@ class Dropdown extends Element {
   detached() {
     parent.removeEventListener('click', this.handleOutsideClicks)
   }
+  initializeSelected(values, list) {
+    const memo = {}
+    for (let [i,v] of list.entries()) {
+      if (values.includes(v))
+        memo[i] = v
+    }
+    return memo
+  }
   onSelect({ target }) {
 
-    let [ul] = [...this.listContainer.children]
-    let listItems = [...ul.children]
-    let index = listItems.indexOf(target)
+    const props = this.props
+    const [ul] = [...this.listContainer.children]
+    const listItems = [...ul.children]
+    const index = listItems.indexOf(target)
+    const selectedValueCount = Object.keys(props.selectedValues.valueOf()).length
 
     if (index < 0) 
       return
 
-    const currentValue = this.selectedByIndex.get(index)
-
-    if (currentValue == null)
-      this.selectedByIndex.set(index, this.items[index])
+    /* problematic: which to deselect ? */
+    const currentValue = props.selectedByIndex.get(index)
+    if (currentValue == null) {
+      props.selectedByIndex.set(index, props.items.get(index))
+    }
     else
-      this.selectedByIndex.undefine(index)
-
-    console.log(currentValue, this.selectedByIndex.valueOf())
-
+      props.selectedByIndex.undefine(index)
 
   }
 
-  position(node, options = { corner: this.corner }) {
+  position(node, options = { corner: this.props.corner }) {
     // return early if there is no parent, to handle case of 'just a dropdown'
 
     const rect = node.getBoundingClientRect()
@@ -175,5 +218,3 @@ class Dropdown extends Element {
   }
 
 }
-
-
