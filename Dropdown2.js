0331060ffe9da4bc1d4ec8eb93098c30e493781f
Alex Ramsdell <alexramsdell@gmail.com>
2019-04-12 09:39:48 -0400

    separate stuff, fix search filter

--- Dropdown2.js
+++ Dropdown2.js
@@ -18,20 +18,6 @@ const {
 
 } = alkali
 
-/*
- * value: 
- * events
- *
- *  maxSelections
- *  maxSelections: Number,
- *  items: [],
- *  searchInputConstructor,
- *  listItemConstructor,
- *
- */
-
-/* selection and filter data */
-
 class Dropdown extends Element.with({ 
 
   children: [ 
@@ -45,33 +31,21 @@ class Dropdown extends Element.with({
 
 }) {
 
-  /*
-   * API:
-   *
-   *  data, ui elements, events
-   *  data: items, value
-   *  ui: input elements 
-   *  events: 
-   *  dre-dd-selected: {},
-   *  dre-dd-removed
-   *
-   *  positioning
-   *
-   */ 
-
-  created({ parent, items, hidden, ItemConstructor, InputConstructor }) {
+  created({ parent=document.body, corner='topright', items, hidden, ItemConstructor, InputConstructor }) {
 
     /* ensure data is reactive */
-    // do i allow a single item? Array.isArray
+
+    // API: do i allow a single item? Array.isArray
     this.items = items instanceof Variable ? items : reactive(items || [])
 
     /* state */
     this.parent = parent
+    this.corner = corner
     this.hidden = hidden || false
     this.searchFilter = new VString('')
     // think there, if new items are added, the active selection reprocesses array
     // array of booleans, indices are list items
-    this.selectedByIndex = items.to(items => reactive(items.map(_ => false)) )
+    this.selectedByIndex = this.items.to(items => reactive(items.map(_ => false)) )
 
     /* pick constructors */
     this.Item = (ItemConstructor || LI).with('.dre-dd-list-item', {
@@ -89,7 +63,6 @@ class Dropdown extends Element.with({
   }
   attached() {
 
-    const parent = this.parent
     const [dropdownContainer] = [...this.children]
     const [searchContainer, listContainer] = [...dropdownContainer.children]
     this.dropdownContainer = dropdownContainer
@@ -112,7 +85,7 @@ class Dropdown extends Element.with({
             hidden: this.searchFilter.to(searchFilter => {
               if (searchFilter.length == 0)
                 return false
-              return !Boolean(itemText.toLowerCase().includes(searchFilter))
+              return !Boolean(itemText.toLowerCase().includes(searchFilter.toLowerCase()))
             }),
             selected: this.selectedByIndex.to(selections => selections[index])
           }
@@ -137,7 +110,7 @@ class Dropdown extends Element.with({
 
   }
 
-  position(node=this.parentNode, options = { corner: 'bottomleft' }) {
+  position(node, options = { corner: this.corner }) {
     // return early if there is no parent, to handle case of 'just a dropdown'
 
     const rect = node.getBoundingClientRect()
@@ -179,63 +152,4 @@ class Dropdown extends Element.with({
 
 }
 
-const block = new Button('.dd-button', { 
-  textContent: 'dropdown', 
-  style: {
-    fontSize: '40px',
-      padding: '50px',
-      border: '1px solid black',
-      bottom: '0px',
-      left: '0px',
-  }
-})
-const items = reactive(['one','two','three'])
-const reasonsRejected = reactive([])
-const D = Dropdown.defineElement('dre-dd')
 
-document.body.appendChild(block)
-
-
-const dd = new D({
-  parent: document.querySelector('.dd-button'),
-  items,
-  hidden: false,
-  //Item: MyListItem
-})
-
-document.body.appendChild(dd)
-
-document.body.appendChild(
-  new Div({
-    style: { 
-      'margin-top': '200px',
-    }
-  }, [ new Span('SEARCH STRING: '), 
-       new Span(dd.searchFilter) ]
-  )
-)
-const filteredSelections = all(dd.selectedByIndex, dd.items)
-                            .to(([xs, items]) => 
-                              xs
-                                .map((x,i) => x ? i : null)
-                                .filter(i => i != null) 
-                                .map(selectedIndex => items[selectedIndex])
-                            )
-
-document.body.appendChild(
-  new Div({
-    style: { 
-    }
-  }, [ 
-    new Span('SELECTIONS: '), 
-    new Span(filteredSelections)
-  ])
-)
-document.body.appendChild(
-  new Div({
-    style: { 
-    }
-  }, [ new Span('SELECTION DATA: '), 
-       new Span(dd.selectedByIndex.to(x => x.join(' | '))) ]
-  )
-)
---
+++ app.js
@@ -0,0 +1,65 @@
+const block = new Button('.dd-button', { 
+  textContent: 'dropdown', 
+  style: {
+    fontSize: '40px',
+      padding: '50px',
+      border: '1px solid black',
+      bottom: '0px',
+      left: '0px',
+  }
+})
+const items = reactive(['one','two','three'])
+const reasonsRejected = reactive([])
+const D = Dropdown.defineElement('dre-dd')
+
+document.body.appendChild(block)
+
+
+const dd = new D({
+  //parent: document.querySelector('.dd-button'),
+  //position: 'bottomleft',
+  items: reactive(data),
+  hidden: false,
+  //Item: MyListItem
+})
+
+document.body.appendChild(dd)
+
+document.body.appendChild(
+  new Div({
+      style: { 
+        'margin-top': '200px',
+      }
+    }, [ 
+      new Span('SEARCH STRING: '), 
+      new Span(dd.searchFilter) 
+    ]
+  )
+)
+const filteredSelections = all(dd.selectedByIndex, dd.items)
+                            .to(([xs, items]) => 
+                              xs
+                                .map((x,i) => x ? i : null)
+                                .filter(i => i != null) 
+                                .map(selectedIndex => items[selectedIndex])
+                            )
+
+document.body.appendChild(
+  new Div({
+    style: { 
+    }
+  }, [ 
+    new Span('SELECTIONS: '), 
+    new Span(filteredSelections.to(x => x.join(' | ')))
+  ])
+)
+/*
+document.body.appendChild(
+  new Div({
+    style: { 
+    }
+  }, [ new Span('SELECTION DATA: '), 
+       new Span(dd.selectedByIndex.to(x => x.join(' | '))) ]
+  )
+)
+*/
